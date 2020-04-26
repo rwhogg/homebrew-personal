617e80b233f544e0d56ec94aefc4cb309c5f5762
Bob Wombat Hogg <wombat@rwhogg.site>
2020-04-26 11:03:15 -0400

    Fixes

--- Formula/cvelookup.rb
+++ Formula/cvelookup.rb
@@ -1,16 +1,16 @@
 class Cvelookup < Formula
   desc "Simple CLI to look up CVE details"
   homepage "https://github.com/rwhogg/cvelookup"
-  head "https://github.com/rwhogg/cvelookup"
+  head "https://github.com/rwhogg/cvelookup.git"
 
-  depends_on "dragonwell8"
+  depends_on "rwhogg/personal-thirdparty/dragonwell8"
   depends_on "gradle" => :build
-  depends_on "unar" => :build
 
   def install
     ENV["JAVA_HOME"] = Formula["dragonwell8"].opt_prefix
     system "gradle", "installDist"
-    libexec.install %w[build/install/golo/bin build/install/golo/docs build/install/golo/lib]
+    libexec.install %w[build/install/cvelookup/bin build/install/cvelookup/lib]
+    bin.install Dir["#{libexec}/bin/*"]
     bin.env_script_all_files libexec/"bin", :JAVA_HOME => "${JAVA_HOME:-#{ENV["JAVA_HOME"]}}"
   end
 
