7e51cda4a54c42e7521b265159e4cd68483932d6
Bob Wombat Hogg <wombat@rwhogg.site>
2020-10-18 16:13:04 -0400

    cvelookup: use openjdk

--- Formula/cvelookup.rb
+++ Formula/cvelookup.rb
@@ -4,12 +4,13 @@ class Cvelookup < Formula
   url "https://github.com/rwhogg/cvelookup/archive/0.1.0.zip"
   sha256 "d2d0eebe557e796bfd9d191552d1aeb5be45d09fb236724d5656faa9e143cfb9"
   head "https://github.com/rwhogg/cvelookup.git"
+  revision 1
 
-  depends_on "rwhogg/personal-thirdparty/dragonwell8"
+  depends_on "openjdk"
   depends_on "gradle" => :build
 
   def install
-    ENV["JAVA_HOME"] = Formula["dragonwell8"].opt_prefix
+    ENV["JAVA_HOME"] = Formula["openjdk"].opt_prefix
     system "gradle", "installDist"
     libexec.install %w[build/install/cvelookup/bin build/install/cvelookup/lib]
     rm_f "#{libexec}/bin/cvelookup.bat"
